0952655c1a3f1e3b247fe80fc4e97be0255ca8d2
Chris R <chris.rocha@>
2017-08-02 21:20:19 -0500

    final commit for the game, working model

--- BasicCard.js
+++ BasicCard.js
@@ -1,10 +1,68 @@
-function BasicCard(front, back){
- if(this instanceof BasicCard){
-     this.front = front;
-     this.back = back;
- } else {
-     return new BasicCard(front, back)
- }
+var inquirer = require('inquirer');
+var ClozeCard = require('./ClozeCard.js');
+var questions = require('./questions.js');
+
+// Create initial variables
+var Questions = [], currentQuestion = 0, correct =0, wrong = 0;
+
+//  Creates the card array
+for (var i = 0; i < questions.length; i++) {
+	var flashcard = new ClozeCard(questions[i].text, questions[i].cloze);
+	Questions.push(flashcard);
 }
 
-module.exports = BasicCard;
+// The function that askes the questions using Inquirer
+function askQuestion() {
+	inquirer.prompt([
+		{
+			type: 'input',
+			// Shows only the partial statement from the constructor
+			message: Questions[currentQuestion].partialText() + '\nAnswer: ',
+			name: 'guess'
+		}
+	]).then(function (answers) {
+		console.log('\n');
+		// Tests if asnwer is correct
+		if (answers.guess.toLowerCase() === Questions[currentQuestion].cloze.toLowerCase()) {
+			console.log('Correct!');
+			// Keeps track of the score
+			correct++;
+		} else {
+			console.log('Wrong');
+			// Keeps track of the score
+			wrong++;
+		}
+		// Shows the correct answer
+		console.log(Questions[currentQuestion].fullText());
+		
+		if (currentQuestion < Questions.length - 1) {
+			currentQuestion++;
+			askQuestion();
+		} else {
+			console.log('Game Over!');
+			console.log('Correct Answers: ' + correct);
+			console.log('Incorrect Answers: ' + wrong);
+
+			// Play again prompt
+			inquirer.prompt([
+				{
+					type: 'confirm',
+					message: 'Had enough?',
+					name: 'Again'
+				}
+			]).then(function (answers) {
+				if (!answers.Again) {
+					// Reset
+					currentQuestion = 0;
+					correct = 0;
+					wrong = 0;
+					askQuestion();
+				} else {
+					console.log('Thanks for playing!');
+				}
+			})
+		}
+	})
+}
+// Initializes the game
+askQuestion();
---
+++ questions.js
@@ -0,0 +1,42 @@
+module.exports = [
+    {
+        text: "Russell Westbrook is the only person other than Oscar Robertson to average a triple double for an entire season",
+        cloze: "Westbrook"
+    },
+    {
+        text: "Michael Jordan won all 6 NBA Finals series he was in.",
+        cloze: "Jordan"
+    },
+    {
+        text: "The Black Mamba is a nickname that is associated with Kobe Bryant",
+        cloze: "Mamba"
+    },
+    {
+        text: "Lebron James has won 3 NBA Championships",
+        cloze: "James"
+    },
+    {
+        text: "The Detroit Pistons are named after this car part",
+        cloze: "Pistons"
+    },
+    {
+        text: "Wilt Chamberlain is famous for being the only player to score 100 points in one basketball game",
+        cloze: "Chamberlain"
+    },
+    {
+        text: "The Dream Team was the name of the first US Olympics Basketball team made up of NBA stars",
+        cloze: "Dream"
+    },
+    {
+        text: "The Orlando Magic were Shaquille O'Neal's first team he played for",
+        cloze: "Magic"
+    },
+    {
+        text: "Phil Jackson is the NBA coach who has won the most NBA Championships",
+        cloze: "Jackson"
+    },
+    {
+        text: "The lakers were originally based in Minneapolis",
+        cloze: "Minneapolis"
+    }
+]
